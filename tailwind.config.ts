84d2acb7cc3ac5c5c922752de706205d27381aea
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2024-10-10 16:59:14 -0700

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+import fluid, { extract, screens, fontSize } from "fluid-tailwind";
+
+export default {
+  content: {
+    files: [
+      "./src/**/*.pug",
+      "./src/**/*.ts",
+      "./src/**/*.js",
+      "./src/**/*.css",
+      "./src/**/*.scss",
+      "./src/**/*.sass",
+    ],
+    extract,
+  },
+  theme: {
+    screens,
+    fontSize,
+    extend: {
+      fontFamily: {
+        playfair: ["PlayfairDisplay", ...fontFamily.sans],
+        montserrat: ["Montserrat", ...fontFamily.sans],
+        formular: ["Formular", ...fontFamily.sans],
+      },
+      colors: {
+        background: "#ECE4DA",
+        text: {
+          DEFAULT: "#37332A",
+          triary: "#74807F",
+        },
+        accent: {
+          DEFAULT: "#B79F7F",
+        },
+        danger: {
+          DEFAULT: "#A55205",
+        },
+      },
+    },
+  },
+  plugins: [fluid],
+} satisfies Config;
